3abae7c63ff57fccc7d608c1569853d827dbae47
chic-page-6502 <zacharytq@gmail.com>
2019-11-12 23:49:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,6 @@
 class Student
   attr_accessor :id, :name, :grade
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
 end
