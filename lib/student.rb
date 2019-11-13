fa1cea9d713020fe91af89d9cbdc8fc8d0889b37
chic-page-6502 <zacharytq@gmail.com>
2019-11-13 00:03:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
-        id PRIMARY KEY INTEGER,
+        id INTEGER PRIMARY KEY,
         name TEXT
       );
       SQL
