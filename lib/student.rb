8709c09f54125803a3e066c631a2767d67353345
chic-page-6502 <zacharytq@gmail.com>
2019-11-13 00:06:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,9 @@ class Student
 
   def self.drop_table
     sql = <<-SQL
-
+      DROP TABLE IF EXISTS students;
+      SQL
+    DB[:conn].execute(sql)
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
