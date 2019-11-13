19bc62a641aca2f012f33f49bb4cc3cd9836348f
chic-page-6502 <zacharytq@gmail.com>
2019-11-13 00:20:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,8 +37,8 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
-  def self.create(name, grade)
-    new_student = Student.new(name, grade)
+  def self.create(thing)
+    new_student = Student.new(thing)
     new_student.save
     new_student
   end
