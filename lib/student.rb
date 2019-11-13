04d1d3042756d3af8776fdd9712e33ce33cb383f
chic-page-6502 <zacharytq@gmail.com>
2019-11-13 00:11:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
       INSERT INTO students (name, grade) VALUES (?, ?);
     SQL
 
-    DB[:conn].execute(sql, @name, @grade)
+    DB[:conn].execute(sql, self.name, self.grade)
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
